4daf0766f519d13cb559128acba7b9dce90fd1da
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-05-19 11:19:21 -0400

    adding jumbotron

---
+++ script.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES 
+// STATE 
+// FUNCTIONS 
+// run the clock - moment & setinterval
+/**
+ * a function that updates the current time every second
+ */
+function runClock() {
+    console.log("runs the clock")
+}
+
+// INTERACTIONS 
+// user clicks add project
+// INITIALIZATION 
