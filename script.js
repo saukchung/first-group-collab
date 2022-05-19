213580869c443a5a60d2441d17ae57c071b02533
theopappas17 <104655034+theopappas17@users.noreply.github.com>
2022-05-19 11:44:45 -0400

    Branch of theo (#5)
    * Add branch
    
    * Pseudocode
    
    * Began form and started JavaScript
    
    Co-authored-by: saukchung <104809363+saukchung@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+
+// DEPENDENCIES
+// STATE
+// FUNCTIONS
+=======
 // DEPENDENCIES 
 // STATE 
 // FUNCTIONS 
+
 // run the clock - moment & setinterval
 /**
  * a function that updates the current time every second
  */
 function runClock() {
+  console.log("runs the clock");
+}
+
+function form() {
+  var projectName = document.getElementById("projName");
+  var projectType = document.getElementById("projType");
+  var dueDate = document.getElementById("datedue");
+  var hourlyRate = document.getElementById("rate");
+}
+
+// INTERACTIONS
+// user clicks add project
+// INITIALIZATION
+=======
     console.log("runs the clock")
 }
 
