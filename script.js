a3d34316f77be74b42e18e4b0854361ba5d50dfc
theopappas17 <theopappas17@gmail.com>
2022-05-19 11:36:24 -0400

    Began form and started JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// DEPENDENCIES 
-// STATE 
-// FUNCTIONS 
+// DEPENDENCIES
+// STATE
+// FUNCTIONS
 // run the clock - moment & setinterval
 /**
  * a function that updates the current time every second
  */
 function runClock() {
-    console.log("runs the clock")
+  console.log("runs the clock");
 }
 
-// INTERACTIONS 
+function form() {
+  var projectName = document.getElementById("projName");
+  var projectType = document.getElementById("projType");
+  var dueDate = document.getElementById("datedue");
+  var hourlyRate = document.getElementById("rate");
+}
+
+// INTERACTIONS
 // user clicks add project
-// INITIALIZATION 
+// INITIALIZATION
